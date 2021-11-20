74d725063dd70034cbdd58da6bbfcc83542ff47e
Akashnani-star <dhanviakash1234@gmail.com>
2021-11-20 06:10:37 +0530

    creating binary tree with python code

---
+++ binaryTreeUsingListData.py
@@ -0,0 +1,46 @@
+class Tree:
+    class Node:
+        def __init__(self,data,left=None,right=None):
+            self.data = data
+            self.left = left
+            self.right = right
+        def __str__(self):
+            return f"{self.left} {self.data} {self.right}"
+    def takeListAndCreateTree(self,listOfValues):
+        root = self.Node(listOfValues[0])
+        i = 1
+        prev = root
+        while(i<len(listOfValues)):
+            if(prev.left is None):
+                prev.left = self.Node(listOfValues[i])
+                i += 1
+                continue
+            elif(prev.right is None):
+                prev.right = self.Node(listOfValues[i])
+                i += 1
+                continue
+            else:
+                prev = prev.left
+        return root
+    @staticmethod
+    def inOrder(root):
+        if(root == None):
+            return
+        Tree.inOrder(root.left)
+        print(root.data,end=" ")
+        Tree.inOrder(root.right)
+    def preOrder(root):
+        if(root == None):
+            return
+        print(root.data,end=" ")
+        Tree.preOrder(root.left)
+        Tree.preOrder(root.right)
+    def postOrder(root):
+        if(root == None):
+            return
+        Tree.postOrder(root.left)
+        Tree.postOrder(root.right)
+        print(root.data,end=" ")
+        
+root = Tree().takeListAndCreateTree([10,20,30,40,50])
+Tree.postOrder(root)
