79ded7f4270dce2d3cf72409f074d33f1634b48e
Akashnani-star <dhanviakash1234@gmail.com>
2021-11-20 06:15:33 +0530

    added comments to binaryTreeUsingListData.py

--- binaryTreeUsingListData.py
+++ binaryTreeUsingListData.py
@@ -1,4 +1,8 @@
+
+# a class to create Tree object
 class Tree:
+
+    # inner class to create nodes in tree
     class Node:
         def __init__(self,data,left=None,right=None):
             self.data = data
@@ -6,6 +10,8 @@ class Tree:
             self.right = right
         def __str__(self):
             return f"{self.left} {self.data} {self.right}"
+
+    # this method takes list and create tree
     def takeListAndCreateTree(self,listOfValues):
         root = self.Node(listOfValues[0])
         i = 1
@@ -22,6 +28,7 @@ class Tree:
             else:
                 prev = prev.left
         return root
+
     @staticmethod
     def inOrder(root):
         if(root == None):
@@ -29,12 +36,16 @@ class Tree:
         Tree.inOrder(root.left)
         print(root.data,end=" ")
         Tree.inOrder(root.right)
+
+    @staticmethod
     def preOrder(root):
         if(root == None):
             return
         print(root.data,end=" ")
         Tree.preOrder(root.left)
         Tree.preOrder(root.right)
+
+    @staticmethod
     def postOrder(root):
         if(root == None):
             return
